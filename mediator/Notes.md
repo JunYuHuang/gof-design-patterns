a20d1c52063eb6bc2a2414e7162433889829009e
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2024-04-13 13:08:05 -0700

    Start WIP on Mediator pattern

---
+++ mediator/Notes.md
@@ -0,0 +1,14 @@
+# Notes
+
+- name: Mediator
+- problem:
+  - TODO
+- solution:
+  - TODO
+- trade-offs:
+  - pros
+    - TODO
+  - cons
+    - TODO
+- misc
+  - TODO
---
+++ mediator/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+// import { sum } from "./index";
+
+describe("Mediator Pattern", () => {
+  test("works", () => {
+    // TODO
+  });
+});
