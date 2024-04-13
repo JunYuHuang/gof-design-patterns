46a46103d407be3db13ef03ac970586b6d5fc22e
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2024-04-13 13:11:39 -0700

    Start WIP on Visitor pattern

---
+++ visitor/Notes.md
@@ -0,0 +1,14 @@
+# Notes
+
+- name: Visitor
+- problem:
+  - TODO
+- solution:
+  - TODO
+- trade-offs:
+  - pros
+    - TODO
+  - cons
+    - TODO
+- misc
+  - TODO
---
+++ visitor/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+// import { sum } from "./index";
+
+describe("Visitor Pattern", () => {
+  test("works", () => {
+    // TODO
+  });
+});
