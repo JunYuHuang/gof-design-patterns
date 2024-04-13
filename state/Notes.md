8f5fcdb347d6e6556148e4c036f2be605010d2bb
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2024-04-13 10:24:58 -0700

    Start WIP on State pattern

---
+++ state/Notes.md
@@ -0,0 +1,14 @@
+# Notes
+
+- name: State
+- problem:
+  - TODO
+- solution:
+  - TODO
+- trade-offs:
+  - pros
+    - TODO
+  - cons
+    - TODO
+- misc
+  - TODO
---
+++ state/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+// import { sum } from "./index";
+
+describe("State Pattern", () => {
+  test("works", () => {
+    // TODO
+  });
+});
