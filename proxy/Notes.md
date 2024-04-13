8cf83792cfbaa67625831ee075e01510848f4dcb
Jun Huang <1624698+JunYuHuang@users.noreply.github.com>
2024-04-13 10:25:13 -0700

    Start WIP on Proxy pattern

---
+++ proxy/Notes.md
@@ -0,0 +1,14 @@
+# Notes
+
+- name: Proxy
+- problem:
+  - TODO
+- solution:
+  - TODO
+- trade-offs:
+  - pros
+    - TODO
+  - cons
+    - TODO
+- misc
+  - TODO
---
+++ proxy/src/index.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, test } from "@jest/globals";
+// import { sum } from "./index";
+
+describe("Proxy Pattern", () => {
+  test("works", () => {
+    // expect(sum(1, 2, 3)).toBe(6);
+  });
+});
